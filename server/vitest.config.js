5e939125809b53f786720b9eff2d1f2ea39d0548
MasindiCodes <masindilukoto@gmail.com>
2026-09-14 01:23:14 +0200

    test: add coverage tooling

--- server/vitest.config.js
+++ server/vitest.config.js
@@ -5,6 +5,10 @@ export default defineConfig({
     env: {
       NODE_ENV: 'test',
     },
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'json'],
+    },
   },
   envDir: '.',
-});
+}); 
